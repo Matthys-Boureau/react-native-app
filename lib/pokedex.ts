c26768ebc6f2386e9ebf6751f97af4dd29ed9460
Matthys Boureau <145988911+MatthysBoureau@users.noreply.github.com>
2025-04-17 16:56:17 +0200

    Limit to 151 pokemon

--- lib/pokedex.ts
+++ lib/pokedex.ts
@@ -1,6 +1,6 @@
 import axiosClient from "./axios";
 
 export async function getPokedex(): Promise<Pokedex> {
-    const { data } = await axiosClient.get<Pokedex>(`/pokemon?limit=1050`);
+    const { data } = await axiosClient.get<Pokedex>(`/pokemon?limit=151&offset=0`);
     return data;
 }
